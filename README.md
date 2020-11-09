e76ab8dfa74008a32c83843df862ea5140e8d562
HJfod <60038575+HJfod@users.noreply.github.com>
2020-11-09 11:09:17 +0200

    upd docs

--- README.md
+++ README.md
@@ -10,52 +10,17 @@
 4. Run `./run.bat c`
 5. You're done! :)
 
+## Usage
+
+[See Usage](file:///D:/github/gdit/docs/usage.html)
+
+## Documentation
+
+[See Documentation](file:///D:/github/gdit/docs/docs.html)
+
 ## TODO
 
- * Add `r` (Release) flag to run.bat (compiles the exe and puts it and all runtimes in a /release folder)
- * Fix that insanely odd bug with importing where it duplicates like 3 levels every time but it's always the same three levels and there's no correlation???
- * Add commits
- * Add merging
+ * Make merge auto-assign groups & colours
  * Add automatic commits / merging
  * Add server support
  * Add ability to merge from server
-
-## Commands
-
-Command template:
-
-```
-./gdit.exe command option <argument> [optional]
-```
-
-#### init
-
-```
-init <level>
-```
-
- * Initializes a new gdit from the level. Replace spaces in the level name with underscores.
-
-#### get
-
-```
-get [<gdit>]
-```
-
- * Get the base level from a gdit. Send this file to your megacollab participants!
-
-#### <path-to-.gditl>
-
-```
-<path-to-.gditl>
-```
-
- * Import the file as a gdit part and start working on it.
-
-#### setup [<setting> [<value>]]
-
-```
-setup [<setting> [<value>]]
-```
-
- * Get / Set a setting.
--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,7 @@ const pages = [
     { name: "Home", file: "index.html" },
     { name: "Installation", file: "install.html" },
     { name: "Basic Usage", file: "usage.html" },
+    { name: "Documentation", file: "docs.html" },
     { name: "About", file: "about.html" }
 ];
 
@@ -12,4 +13,25 @@ pages.forEach(page => {
     if (window.location.href.endsWith(page.file)) but.classList.add("nav-selected");
     but.setAttribute("onclick", `window.location.href = "${page.file}"`);
     document.getElementById("main-nav").appendChild(but);
+
+    if (document.querySelector("#page-nav") != null) {
+        const li = document.createElement("li");
+        li.innerHTML = `<a href="${page.file}">${page.name}</a>`;
+        document.querySelector("#page-nav").appendChild(li);
+    }
 });
+
+const navtog = document.createElement("button");
+navtog.innerHTML = "≡";
+navtog.classList.add("nav-toggle");
+navtog.addEventListener("click", e => {
+    const n = document.getElementsByTagName("nav")[0];
+    n.hasAttribute("off") ? n.removeAttribute("off") : n.setAttribute("off", "");
+});
+document.getElementsByTagName("header")[0].insertBefore(navtog, document.querySelector("header text"));
+
+document.getElementById("main-nav").innerHTML += "<footer><text>GDit © HJfod 2020.</text></footer>";
+
+if (document.querySelector("#license-text") != null)
+    fetch("https://raw.githubusercontent.com/HJfod/gdit/master/LICENSE").then(res => res.text().then(t =>
+        document.querySelector("#license-text").innerHTML = t.replace(/\n/g, "<br>")));
